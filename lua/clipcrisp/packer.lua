53b488ce3a9e5ebf05efcb24ab3b868943173f00
Cory Stewart <corystewart@Corys-Mac-mini.local>
2023-03-18 16:17:36 -0400

    Remove some comments

--- lua/clipcrisp/packer.lua
+++ lua/clipcrisp/packer.lua
@@ -46,9 +46,6 @@ return require('packer').startup(function(use)
         requires = "nvim-lua/plenary.nvim",
         config = function()
             require("todo-comments").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
             }
         end
     }
