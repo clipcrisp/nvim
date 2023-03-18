431b723af27fe4e54635ed4b8955e89d5f871499
corywstewart99@gmail.com <corywstewart99@gmail.com>
2023-03-18 18:23:38 -0400

    Add lualine to config

--- lua/clipcrisp/packer.lua
+++ lua/clipcrisp/packer.lua
@@ -49,4 +49,9 @@ return require('packer').startup(function(use)
             }
         end
     }
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 end)
