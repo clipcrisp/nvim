64dd0e3fb201302ddea558447e0fd9267f03e0e0
corywstewart99@gmail.com <corywstewart99@gmail.com>
2023-03-19 22:56:34 -0400

    Add keymap for toggleterm

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm direction=float<CR>")
 vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
+vim.keymap.set('t', '<leader>tt', "<cmd>ToggleTerm direction=float<CR>")
