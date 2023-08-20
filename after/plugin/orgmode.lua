f0ea9f3adf9b04c9bd2769db0252d58cee7acdd3
Cory Stewart <corystewart@Corys-MacBook-Pro.local>
2023-08-20 13:21:02 -0400

    feat: add org capture templates and python run keybind

--- after/plugin/orgmode.lua
+++ after/plugin/orgmode.lua
@@ -3,18 +3,28 @@ require('orgmode').setup_ts_grammar()
 
 -- Treesitter configuration
 require('nvim-treesitter.configs').setup {
-  -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-  -- highlighting will fallback to default Vim syntax highlighting
-  highlight = {
-    enable = true,
-    -- Required for spellcheck, some LaTex highlights and
-    -- code block highlights that do not have ts grammar
-    additional_vim_regex_highlighting = {'org'},
-  },
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
+    -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+    -- highlighting will fallback to default Vim syntax highlighting
+    highlight = {
+        enable = true,
+        -- Required for spellcheck, some LaTex highlights and
+        -- code block highlights that do not have ts grammar
+        additional_vim_regex_highlighting = {'org'},
+    },
+    ensure_installed = {'org'}, -- Or run :TSUpdate org
 }
 
 require('orgmode').setup({
-  org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
-  org_default_notes_file = '~/Dropbox/org/refile.org',
+    org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
+    org_default_notes_file = '~/Dropbox/org/refile.org',
+
+    org_capture_templates = {
+        t = { description = 'Task', template = '* TODO %?\n  %u' },
+        g = { description = 'Generic', template = '%?\n  %u' },
+        j = {
+            description = 'Journal',
+            template = '\n* %<%Y-%m-%d> %<%A>\n** %U\n\n%?',
+            target = '~/sync/org/journal.org'
+        }
+    }
 })
--- lua/clipcrisp/remap.lua
+++ lua/clipcrisp/remap.lua
@@ -1,6 +1,16 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>zf", function()
+vim.keymap.set("n", "<leader>zr", function()
     vim.cmd(":! zig run %")
+end)
+vim.keymap.set("n", "<leader>pr", function()
+    vim.cmd(":! python3 %")
+end)
+
+vim.keymap.set("n", "<leader>t1", function()
+    vim.api.nvim_feedkeys("<C-t>1", "n", true)
+end)
+vim.keymap.set("n", "<leader>t2", function()
+    vim.fn.feedkeys("<C-t>2")
 end)
